ee570fdc8d9f09b3b308f1da2a4b02f378dbcfb5
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2025-09-22 09:21:48 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyAza98u8-NVn9hNbuLwcsaCZX2hXbtVaHk",
+  authDomain: "meu-app-de-login.firebaseapp.com",
+  projectId: "meu-app-de-login",
+  storageBucket: "meu-app-de-login.appspot.com",
+  messagingSenderId: "61119567504",
+  appId: "1:61119567504:web:556bb893c9eba6c4e12a15",
+  measurementId: "G-YY6QTZX57K"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+console.log("App.js carregado com sucesso.");
