f9daa80e1c51e376fd36c1f8b54ba0be0ceb3587
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2025-09-22 13:18:13 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,9 @@
 // Inicialização do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyAza98u8-NVn9hNbuLwcsaCZX2hXbtVaHk",
   authDomain: "meu-app-de-login.firebaseapp.com",
   projectId: "meu-app-de-login",
-  storageBucket: "meu-app-de-login.appspot.com",
+  storageBucket: "meu-app-de-login.firebasestorage.app",
   messagingSenderId: "61119567504",
   appId: "1:61119567504:web:556bb893c9eba6c4e12a15"
 };
