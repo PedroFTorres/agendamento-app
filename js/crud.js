5be6d233bec3f17875eac4debe8bc1a6d23cc505
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2026-04-15 22:13:49 -0300

    Update crud.js

--- js/crud.js
+++ js/crud.js
@@ -428,21 +428,7 @@ if (type === "clientes") {
     e.target.value = e.target.value.replace(/\D/g, "");
   });
 }
-  cnpjInput?.addEventListener("input", (e) => {
-  let v = e.target.value.replace(/\D/g, ""); // 🔒 remove tudo que não é número
-  v = v.replace(/^(\d{2})(\d)/, "$1.$2");
-  v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
-  v = v.replace(/\.(\d{3})(\d)/, ".$1/$2");
-  v = v.replace(/(\d{4})(\d)/, "$1-$2");
-  e.target.value = v;
-});
-
-  // 🔹 MÁSCARA CEP
-  cepInput?.addEventListener("input", (e) => {
-    let v = e.target.value.replace(/\D/g, "");
-    v = v.replace(/^(\d{5})(\d)/, "$1-$2");
-    e.target.value = v;
-  });
+ 
 
   const list = document.getElementById(`${type}-list`);
 
